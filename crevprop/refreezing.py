73f149ab3e192d6938332033357bf4232d9ce0a7
Jessica Mejia <jzmejia990@gmail.com>
2021-11-04 13:11:20 -0400

    updates

--- crevprop/refreezing.py
+++ crevprop/refreezing.py
@@ -7,3 +7,10 @@ adapted from K. Poinar's matlab script VfrzForMoulin.m
 adapted by J. Mejia
 last update Nov 2021
 """
+import numpy as np
+
+
+# <NOTE: The following function crevasse volume will likely be
+#   implemented as a class method instead of a stand-alone function.
+#   writing as a function for now as I am still working out the class
+#   structure and inheratance >
