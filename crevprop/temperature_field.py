545a7aa73f389d7a0e405de6e6335908f0507dba
Jessica Mejia <jzmejia990@gmail.com>
2022-11-30 18:56:23 -0500

    temperature solver implemented

--- crevprop/temperature_field.py
+++ crevprop/temperature_field.py
@@ -286,19 +286,21 @@ class ThermalModel():
             crev_idx : list of crevasse indicies within thermal model
         '''
         crev_idx = []
+
         for crev in self.crevasses:
             # crevasse index at ice surface
             crev_x = (self.x.size * self.z.size - 1) - \
                 abs(round(crev[0]/self.dx))
 
             # find depth indicies for crevasse
-            crev_depth = crev[1]
-            if crev_depth >= 2*self.dz and crev_depth < self.ice_thickness:
+            if crev[1] >= 2*self.dz and crev[1] < self.ice_thickness:
                 crev_idx.extend(np.arange(
-                    crev_x - (np.floor(crev_depth/self.dz) - 1) * self.x.size,
+                    crev_x -
+                    (abs(np.floor(crev[1]/self.dz)) - 1) * self.x.size,
                     crev_x, self.x.size))
             else:
                 crev_idx.extend([crev_x])
+
         self.crev_idx = crev_idx
         return crev_idx
 
@@ -318,118 +320,28 @@ class ThermalModel():
 
         # compute rhs
         rhs = self.T.flatten()
-        rhs[:, self.crev_idx] = self.Tcrev
+
+        # apply boundary conditions
+        rhs[self.crev_idx] = self.T_crev
         rhs[:nx] = self.T_bed
         rhs[-nx:] = self.T_surface
-        rhs[:, np.arange(nx, self.T.size-nx, nx)] = self.T_upglacier
-        rhs[:, np.arange(2*nx-1, self.T.size-nx, nx)] = self.T_downglacier
+        rhs[np.arange(nx, self.T.size-nx, nx)] = self.T_upglacier[1:-1]
+        rhs[np.arange(2*nx-1, self.T.size-nx, nx)] = self.T_downglacier[1:-1]
 
-        pass
+        # add source term near crevasses
+        # upstream of crevasse
+        # rhs[crev_idx - 1] = rhs[crev_idx -1] + (Lf/B * virtualblue[])/dx
+        
+        # downstream of creasse
+        # rhs[crev_idx + 1] = rhs[crev_idx +1] + (Lf/B * virtualblue[])/dx
+        
+        
+        # compute solution vector
+        T = np.linalg.solve(A,rhs)
+        return T.reshape(self.T.shape)
 
     def refreezing(self):
         bluelayer = self.dt * pc.THERMAL_CONDUCTIVITY_ICE / \
             (pc.LATIENT_HEAT_OF_FUSION * DENSITY_ICE) * ()
         pass
-
-
-# General thermal equations for pure ice and glacier ice
-
-class PureIce(object):
-    """Thermal equations applicable to pure ice.
-
-    Parameters
-    ----------
-    Temperature: float, int
-        Ice temperature in degrees Kelvin (K).
-
-    Attributes
-    ----------
-    T: float, int
-        ice temperature in degrees Kelvin (K)
-    Lf: float
-        Latient heat of fusion for ice
-    density: float
-        density of pure ice
-
-    """
-
-    def __init__(self, Temperature):
-        """
-        Parameters
-        ----------
-        Temperature : float
-            ice temperature in degree K.
-
-        """
-        self.T = Temperature
-        self.Lf = 333.5
-        self.density = 9.17
-        self.specific_heat_capacity = self.specific_heat_capacity()
-        self.thermal_conductivity = self.thermal_conductivity()
-        self.thermal_diffusivity = self.thermal_conductivity / (
-            self.density * self.specific_heat_capacity)
-
-    def specific_heat_capacity(self):
-        """specific heat capacity for pure ice (J/kg/K)
-
-        Specific heat capacity, c, per unit mass of ice in SI units. 
-        Note: c of dry snow and ice does not vary with density 
-        because the heat needed to warm the air and vapor between 
-        grains is neglibible. (see Cuffey, ch 9, pp 400)
-
-        c = 152.5 + 7.122(T)
-
-        Parameters
-        ----------
-        temperature: float
-            ice temperature in degrees Kelvin
-
-        Returns
-        -------
-        c: float 
-            specific heat capacity of ice in Jkg^-1K^-1
-
-        """
-
-        return 152.5 + 7.122 * self.T
-
-    def thermal_conductivity(self):
-        """calc thermal conductivy for pure ice (W/m/K)"""
-        return 9.828 * np.exp(-5.7e-3 * self.T)
-
-
-# for not pure ice
-def thermal_conductivity(density):
-    """Depth dependant thermal conductivity for dry snow, firn, and ice
-    Van Dusen 1929
-
-    This equation typically gives a lower limit in most cases
-
-    Parameters
-    ----------
-    density : (float)
-        density of dry snow, firn, or glacier ice in kg/m^3
-
-    """
-    return 2.1e-2 + 4.2e-4 * density + 2.2e-9 * density**3
-
-
-def thermal_diffusivity(thermal_conductivity, density, specific_heat_capacity):
-    """ Thermal diffusivity calculation
-
-    Parameters
-    ----------
-    thermal_conductivity : (float)
-        in W/mK
-    density : (float) 
-        kg/m^3
-    specific_heat_capacity : (float)
-        J/kgK
-
-    Returns
-    -------
-    thermal_diffusivity : (float)
-        units of m^2/s
-
-    """
-    return thermal_conductivity/(density * specific_heat_capacity)
+    
